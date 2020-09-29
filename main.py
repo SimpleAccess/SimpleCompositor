a6f4fcb43d2f95e1ea2679ae3a830536bb31bd03
Ruben <runlevelzero@users.noreply.github.com>
2020-09-29 09:48:31 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from functools import partial
+import signal
+
+from pywayland import lib
+from pywayland.server import Display
+
+from wlroots.backend import Backend
+from wlroots.renderer import Renderer
+
+from compositor import Compositor
+
+def signalInt(display, signalNumber, frame):
+    print("SIGINT Received")
+    display.destroy()
+
+def main():
+    with Display() as display:
+        signal.signal(signal.SIGINT, partial(signalInt, display))
+        with Backend(display) as backend:
+            renderer = Renderer(backend, display)
+            compositor = Compositor(display, backend, renderer)
+            backend.start()
+            display.run()
+            display.destroy()
+
+main()
